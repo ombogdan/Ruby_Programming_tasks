ccce4a69d9ea1a3c4178b9dc36bb06aa5b91c60d
ombogdan <omelcenko44@gmail.com>
2019-10-15 12:31:51 +0300

    Add 9 part

---
+++ src/9-part/272-task.rb
@@ -0,0 +1,9 @@
+def task_272()
+  a = 0.0
+  for i in 1901..1950
+    a += i
+    a_average = a/50
+  end
+  puts a_average
+end
+task_272()
---
+++ src/9-part/275-task.rb
@@ -0,0 +1,13 @@
+def task_275()
+  a = 0.0
+  for x in 1..10
+    for y in 1..10
+      a += x*y
+    end
+  end
+  puts a
+
+end
+
+task_275()
+
